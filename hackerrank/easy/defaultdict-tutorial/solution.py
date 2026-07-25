d446a931d052cb8343e1eeb80953d97161599da4
Geetha Poloju <23ag1a7250@gmail.com>
2026-07-25 14:01:53 +0530

    ✅ DefaultDict Tutorial | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-25

---
+++ hackerrank/easy/defaultdict-tutorial/solution.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import defaultdict
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    A = [input() for x in range(n)]
+    B = [input() for x in range(m)]
+    ID = defaultdict(list)
+    for i, x in enumerate(A):
+        ID[x].append(i+1)
+    for x in B:
+        if x in ID:
+            print(' '.join(map(str, ID[x])))
+        else:
+            print(-1)
